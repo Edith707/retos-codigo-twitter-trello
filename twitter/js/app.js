33c41c496ce7f172382d920845247e3a283786c8
Edith <edith.lopezhuerta@gmail.com>
2017-12-30 02:51:10 -0600

    construyendo un contador

--- twitter/js/app.js
+++ twitter/js/app.js
@@ -3,6 +3,32 @@ var account= document.getElementById("account");
 var textArea = document.getElementById("tweet");//textArea
 var button = document.getElementById("twittear"); //button tweet
 
+//Funcion para generar la variable tiempo
+var time = function (event){
+  var toDay = new Date();
+  var hour = toDay.getHours();
+  var min = toDay.getMinutes();
+  var ampm = "";
+  //Condicional para ver si se utiliza am o pm
+  if(hour >= 12){
+    hour = hour - 12;
+    ampm = "PM";
+  }else{
+    ampm = "AM";
+  };
+
+  if(hour === 0){
+    hour = 12;
+  };
+
+  if(min < 10){
+    min = "0" + min;
+  };
+
+ var timeNow =hour + ":" + min+ ampm;
+ return timeNow
+}
+
 var addTweet = function (event) {
   //Taer las tags necesarias para la funcion
   var textArea = document.getElementById("tweet");//textArea
@@ -20,16 +46,44 @@ var addTweet = function (event) {
   tweet.appendChild(conteinerTweet);
   tweet.appendChild(conteinerHour);
   conteinerTweets.appendChild(tweet);
- console.log(tweet);
 
 };
 
 var accountLetters = function (event) {
-  var account= document.getElementById("account");
+  var account= document.getElementById("account").innerText;  //contador (p)
+  var textArea = document.getElementById("tweet");// Tag del textarea
+  var textLength = textArea.value.length; //longitud del texto
+  var counter = account.value =140 -textLength; //contador de caracteres
+  var button = document.getElementById("twittear"); //buton Tweet
+/*
+  if(textLength > 140){
+    button.disabled = true;
+    button.style.background = "#EEA5CC";
+    counter.style.color = "red";
+  }else if(textLength >= 130){
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "orange";
+  }else if(textLength >= 120){
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "green";
+  }else if(textLength <= 0){
+    button.disabled = true;
+    button.style.background= "#EEA5CC";
+  }else{
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "gray";
+
+*/
+  console.log(counter);
 
 };
 
-//Agregando eventos a las variables
 
-button.addEventListener("click", addTweet);
-textArea.addEventListener("keyup", accountLetters);
+
+//Agregando eventos a las variables
+button.addEventListener("click", time); //Agregando evento al button twitter y ejecutar la función time
+button.addEventListener("click", addTweet); //Agregando evento al button twitter y ejecutar la función addTweet
+textArea.addEventListener("keyup", accountLetters); //Agregando evento al button twitter y ejecutar la función contador de letras
