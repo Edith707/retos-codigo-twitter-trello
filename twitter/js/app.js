3dc25af9088ffe08a1c643e7532c3a6dfde6b89b
Edith <edith.lopezhuerta@gmail.com>
2017-12-31 02:58:32 -0600

    mejorando un poco twitter

--- twitter/js/app.js
+++ twitter/js/app.js
@@ -26,64 +26,66 @@ var time = function (event){
   };
 
  var timeNow =hour + ":" + min+ ampm;
- return timeNow
+  console.log(timeNow);
+  return timeNow;
 }
 
-var addTweet = function (event) {
+var addTweet = function (event, timeNow) {
   //Taer las tags necesarias para la funcion
   var textArea = document.getElementById("tweet");//textArea
   var button = document.getElementById("twittear"); //button tweet
-  var conteinerTweets = document.getElementById("conteiner_tweets"); //contenedor de new tweets
+  var conteinerNew = document.getElementById("conteiner_tweets"); //contenedor de new tweets
   var text = textArea.value;
   //Creando nuevos elementos
    var tweet = document.createElement("div");//div
     tweet.className ="tweet";//dando clase al div
    var conteinerTweet = document.createElement("p");//p
+     conteinerTweet.className ="tweet2";
    var conteinerHour = document.createElement("span");//span
-   var conteinerText =  document.createTextNode(text);//node text
-  //Agregar los nuevos elementos al html
-  conteinerTweet.appendChild(conteinerText);
-  tweet.appendChild(conteinerTweet);
-  tweet.appendChild(conteinerHour);
-  conteinerTweets.appendChild(tweet);
+  var nodoText  = document.createTextNode(text);//node text
+  var time = document.createTextNode(timeNow);
 
+
+  /*//Agregar los nuevos elementos al html
+   conteinerHour.appendChild(time);
+   conteinerTweet.appendChild(nodoText);
+   tweet.appendChild(conteinerTweet);
+   tweet.appendChild(conteinerHour);
+   conteinerNew.appendChild(tweet);*/
 };
 
 var accountLetters = function (event) {
-  var account= document.getElementById("account").innerText;  //contador (p)
-  var textArea = document.getElementById("tweet");// Tag del textarea
+  var account= document.getElementById("account");// //contador (p)
   var textLength = textArea.value.length; //longitud del texto
   var counter = account.value =140 -textLength; //contador de caracteres
-  var button = document.getElementById("twittear"); //buton Tweet
-/*
-  if(textLength > 140){
+   account.innerText = counter;
+ console.log(counter);
+//Condicionales para desabilitar el boton tweet y para cambiar el color del acoount
+  if(textLength >= 140){
     button.disabled = true;
-    button.style.background = "#EEA5CC";
-    counter.style.color = "red";
+    button.style.background =  "#FF5733";
   }else if(textLength >= 130){
-    button.disabled = false;
-    button.style.background = "#D61E80";
-    counter.style.color = "orange";
+    button.style.background = "#1DA1F2";
+    account.style.color = "#f39709 ";
   }else if(textLength >= 120){
     button.disabled = false;
-    button.style.background = "#D61E80";
-    counter.style.color = "green";
+    button.style.background = "#1DA1F2";
+    account.style.color = " #61cd08";
   }else if(textLength <= 0){
     button.disabled = true;
-    button.style.background= "#EEA5CC";
+    account.style.background= "##1DA1F2";
   }else{
-    button.disabled = false;
-    button.style.background = "#D61E80";
-    counter.style.color = "gray";
-
-*/
-  console.log(counter);
-
+   account.style.background= "none";
+   button.disabled = false;
+  }
 };
 
-
-
+//Funcion para que cada vez que recargo la página se limpie el textarea
+var inner = function (event){
+  textArea.value = "";
+};
 //Agregando eventos a las variables
 button.addEventListener("click", time); //Agregando evento al button twitter y ejecutar la función time
 button.addEventListener("click", addTweet); //Agregando evento al button twitter y ejecutar la función addTweet
 textArea.addEventListener("keyup", accountLetters); //Agregando evento al button twitter y ejecutar la función contador de letras
+window.addEventListener("load", inner); // Agregando evento al cargar la página
